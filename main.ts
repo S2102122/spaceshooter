8dc7bca0430ad0c3aa80fde489663e40fc548688
S2102122 <117302078+S2102122@users.noreply.github.com>
2023-02-28 14:38:26 +0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
-    Bullet = game.createSprite(SpaceShip.get(LedSpriteProperty.X), SpaceShip.get(LedSpriteProperty.Y))
-    Bullet.turn(Direction.Left, 90)
+    if (canShoot) {
+        Bullet = game.createSprite(SpaceShip.get(LedSpriteProperty.X), SpaceShip.get(LedSpriteProperty.Y))
+        Bullet.turn(Direction.Left, 90)
+        canShoot = false
+    }
 })
 function Crash () {
     if (Bullet.isTouching(Enemy)) {
@@ -8,13 +11,20 @@ function Crash () {
         Bullet.delete()
         Enemy.delete()
         Enemy = game.createSprite(randint(0, 4), 0)
+        canShoot = true
+    } else if (Bullet.get(LedSpriteProperty.Y) == 0) {
+        Bullet.delete()
+        canShoot = true
     }
 }
 let Bullet: game.LedSprite = null
+let canShoot = false
 let Enemy: game.LedSprite = null
 let SpaceShip: game.LedSprite = null
 SpaceShip = game.createSprite(2, 4)
 Enemy = game.createSprite(randint(0, 4), 0)
+canShoot = true
+game.startCountdown(20000)
 basic.forever(function () {
     SpaceShip.move(1)
     SpaceShip.ifOnEdgeBounce()
